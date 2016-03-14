cc8aef50b0cf581538b2f9401f9d40c7e07fd64d
Eleanor Miller <MillerLElle@gmail.com>
2016-03-14 11:17:26 -0400

    GFEJJ-1 set up project classes and refrences

---
+++ Flooring/Flooring.BLL/Minipulate Order/DisplayOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flooring.BLL.Minipulate_Order
+{
+    public class DisplayOrder
+    {
+    }
+}
