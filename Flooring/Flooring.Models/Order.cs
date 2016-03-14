6a6c2825d3f6437688ae647c52a4ce43466dc840
Hyun Jeong Hong <hisuida@me.com>
2016-03-14 16:14:41 -0400

    GFEJJ-1 added order model

---
+++ Flooring/Flooring.Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flooring.Models
+{
+   public class Order
+    {
+        public int OrderNumber { get; set; }
+        public int OrderArea { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProductType { get; set; }
+        public string StateAbbr { get; set; }
+        public string StateFull { get; set; }
+        public decimal CostperSqFt { get; set; }
+        public decimal LaborperSqFt { get; set; }
+        public decimal TaxRate { get; set; }
+        public DateTime OrderDate { get; set; }
+
+    }
+}
