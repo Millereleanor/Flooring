dcc393b27eae9ff3d7cb165e4c1c917ed8ab0e17
Eleanor Miller <MillerLElle@gmail.com>
2016-03-15 11:08:51 -0400

    GFEJJ-10 ADDED FILE STUFF....AND REPO

--- Flooring/Flooring.Data/FloorRepository.cs
+++ Flooring/Flooring.Data/FloorRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -12,43 +13,70 @@ namespace Flooring.Data
     {
         //todo:convert this to dictionary version
 
-        public Dictionary<DateTime, Order> Order;
+        public Dictionary<DateTime, Order> Decode()
+        {
+            Dictionary<DateTime, Order> order = new Dictionary<DateTime, Order>();
 
-        //private static List<Orders> Decode()
-        //{
-        //    List<Orders> orders = new List<Account>();
-        //    string filePath = ConfigurationManager.AppSettings["FileName"];
+            string filePath = ConfigurationManager.AppSettings["FileName"];
 
-        //    var reader = File.ReadAllLines(filePath);
+            var reader = File.ReadAllLines(filePath);
 
-        //    for (int i = 1; i < reader.Length; i++)
-        //    {
-        //        var columns = reader[i].Split(',');
-        //        var order = new Order();
-        //        order.OrderNumber = int.Parse(columns[0]);
-        //        order.FirstName = columns[1];
-        //        order.LastName = columns[2];
-        //        //order. = decimal.Parse(columns[3]);
+            for (int i = 1; i < reader.Length; i++)
+            {
+                var columns = reader[i].Split(',');
 
-        //        Orders.Add(Order);
-        //    }
-        //    return Orders;
-        //}
+                var newOrder = new Order();
+                var Name = newOrder.FirstName + " " + newOrder.LastName;
+                
+                newOrder.OrderNumber = int.Parse(columns[0]);
+                newOrder.OrderArea = int.Parse(columns[6]);
+                Name = columns[2];
+                newOrder.ProductType;
+                newOrder.
+                //order. = decimal.Parse(columns[3]);
 
-        //public void WriteToFile(List<Order> orders)
-        //{
-        //    string filePath = ConfigurationManager.AppSettings["FileName"];
-        //    using (StreamWriter writer = new StreamWriter(filePath, false))
-        //    {
-        //        {
-        //            writer.WriteLine("");
+                Order.Add(Order);
+            }
+            return Order;
+        }
 
-        //            foreach (var order in orders)
-        //            {
-        //                writer.WriteLine("{0},{1},{2},{3}", order.OrderNumber,order.OrderArea,order.OrderDate);
-        //            }
-        //        }
-        ////    }
-        //}
+
     }
+    //private static List<Orders> Decode()
+    //{
+    //    List<Orders> orders = new List<Account>();
+    //    string filePath = ConfigurationManager.AppSettings["FileName"];
+
+    //    var reader = File.ReadAllLines(filePath);
+
+    //    for (int i = 1; i < reader.Length; i++)
+    //    {
+    //        var columns = reader[i].Split(',');
+    //        var order = new Order();
+    //        order.OrderNumber = int.Parse(columns[0]);
+    //        order.FirstName = columns[1];
+    //        order.LastName = columns[2];
+    //        //order. = decimal.Parse(columns[3]);
+
+    //        Orders.Add(Order);
+    //    }
+    //    return Orders;
+    //}
+
+    //public void WriteToFile(List<Order> orders)
+    //{
+    //    string filePath = ConfigurationManager.AppSettings["FileName"];
+    //    using (StreamWriter writer = new StreamWriter(filePath, false))
+    //    {
+    //        {
+    //            writer.WriteLine("");
+
+    //            foreach (var order in orders)
+    //            {
+    //                writer.WriteLine("{0},{1},{2},{3}", order.OrderNumber,order.OrderArea,order.OrderDate);
+    //            }
+    //        }
+    ////    }
+    //}
+}
 }
