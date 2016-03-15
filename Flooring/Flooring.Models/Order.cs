42c2046325b33817d7cdfc64ea33322b198bc64e
Hyun Jeong Hong <hisuida@me.com>
2016-03-15 19:10:23 -0400

    GFEJJ-2 Fixed some discrepancies

--- Flooring/Flooring.Models/Order.cs
+++ Flooring/Flooring.Models/Order.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace Flooring.Models
 {
-   public class Order
+    public class Order
     {
-       private decimal _orderTotal;
-       public int OrderNumber { get; set; }
+        private decimal _orderTotal;
+        public int OrderNumber { get; set; }
         public int OrderArea { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -19,16 +19,18 @@ namespace Flooring.Models
         public decimal CostperSqFt { get; set; }
         public decimal LaborperSqFt { get; set; }
         public decimal TaxRate { get; set; }
+
         public decimal TaxTotal
         {
             get { return this.TaxRate*this.OrderTotal; }
         }
 
-       public decimal OrderTotal
-       {
-           get { return CostperSqFt*OrderArea + LaborperSqFt*OrderArea; }
-           
-       } 
-       public DateTime OrderDate { get; set; }
+        public decimal OrderTotal
+        {
+            get { return CostperSqFt*OrderArea + LaborperSqFt*OrderArea; }
+
+        }
+
+        public DateTime OrderDate { get; set; }
     }
 }
--- Flooring/Flooring.UI/Workflows/EditOrderWorkflow.cs
+++ Flooring/Flooring.UI/Workflows/EditOrderWorkflow.cs
@@ -12,90 +12,89 @@ namespace Flooring.UI.Workflows
 {
     public class EditOrderWorkflow
     {
-        public void Execute()
-        {
-            GetOrderDateFromUser();
-            DisplayOrderbyDate();
+        //public void Execute()
+        //{
+        //    GetOrderDateFromUser();
+        //    DisplayOrderbyDate();
 
-        }
+        //}
 
-        private DateTime GetOrderDateFromUser()
-        {
-            do
-            {
-                Console.Clear();
-                Console.Write("Please enter the order date: ");
-                string dateoforder = Console.ReadLine();
+        //private DateTime GetOrderDateFromUser()
+        //{
+        //    do
+        //    {
+        //        Console.Clear();
+        //        Console.Write("Please enter the order date: ");
+        //        string dateoforder = Console.ReadLine();
 
-                if (DateTime.TryParse(dateoforder, out Date))
-                {
-                    return Date;
-                }
-                Console.WriteLine("That is not a valid date");
-                Console.WriteLine("Press enter to continue...");
-                Console.ReadLine();
-            } while (true);
-        }
+        //        if (DateTime.TryParse(dateoforder, out Date))
+        //        {
+        //            return Date;
+        //        }
+        //        Console.WriteLine("That is not a valid date");
+        //        Console.WriteLine("Press enter to continue...");
+        //        Console.ReadLine();
+        //    } while (true);
+        //}
 
-        private void DisplayOrderbyDate(DateTime Date)
-        {
-            var ops = new OrderOperations();
-            var response = ops.GetOrders(Date);
+        //private void DisplayOrderbyDate(DateTime Date)
+        //{
+        //    var ops = new OrderOperations();
+        //    var response = ops.GetOrders(Date);
 
 
-            if (response.Success)
-            {
-                _currentOrder = response.OrderInfo;
-                PrintOrderInformation();
+        //    if (response.Success)
+        //    {
+        //        _currentOrder = response.OrderInfo;
+        //        PrintOrderInformation();
 
-            }
-            else
-            {
-                Console.WriteLine("Error: ");
-                Console.WriteLine(response.Message);
-                Console.WriteLine("Move along...");
-                Console.ReadLine();
-            }
-        }
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Error: ");
+        //        Console.WriteLine(response.Message);
+        //        Console.WriteLine("Move along...");
+        //        Console.ReadLine();
+        //    }
+        //}
 
-        private void PrintOrderInformation()
-        {
-            Console.Clear();
-            Console.WriteLine("Order Information: ");
-            Console.WriteLine("=====================================");
-            Console.WriteLine("Order Number: {0}", _currentOrder.OrderNumber);
-            Console.WriteLine("Customer Name: {0},{1}", _currentOrder.LastName, _currentOrder.FirstName);
-            Console.WriteLine("Order State: {0}, ({1})      State Tax Rate: {3}",
-                _currentOrder.StateAbbr, _currentOrder.StateFull, _currentOrder.TaxRate);
-            Console.WriteLine("Product Type: {0}     Order Area: {1}", _currentOrder.ProductType, _currentOrder.OrderArea);
-            Console.WriteLine("Material Cost per Ft^2: {0}        Labor Cost per Ft^2: {1}",
-                _currentOrder.CostperSqFt, _currentOrder.LaborperSqFt);
-            Console.WriteLine("Order Total: {0}", _currentOrder.Total);
-            Console.WriteLine();
+        //private void PrintOrderInformation()
+        //{
+        //    Console.Clear();
+        //    Console.WriteLine("Order Information: ");
+        //    Console.WriteLine("=====================================");
+        //    Console.WriteLine("Order Number: {0}",order.OrderNumber);
+        //    Console.WriteLine("Customer Name: {0},{1}", order.LastName, order.FirstName);
+        //    Console.WriteLine("Order State: {0}, ({1})      State Tax Rate: {3}",
+        //        order.StateAbbr, order.StateFull, order.TaxRate);
+        //    Console.WriteLine("Product Type: {0}     Order Area: {1}", order.ProductType, order.OrderArea);
+        //    Console.WriteLine("Material Cost per Ft^2: {0}        Labor Cost per Ft^2: {1}",
+        //        order.CostperSqFt, order.LaborperSqFt);
+        //    Console.WriteLine("Order Total: {0}", order.Total);
+        //    Console.WriteLine();
 
-        }
+        //}
 
           
 
 
-            //In Edit Method
-            //TODO: Ask User for a date
-            //TODO: Ask User for order
-            //TODO: Check to see if date and # are valid (Possibly in validInput(dateTime, int))
-            //TODO: Call GetOrder() in DisplayWorkflow
-            //TODO: Display the Order recieved
-            //TODO: Displays the variables to be edited (Name, State, Product Type, Area
-            //TODO: Asks User if they want to edit or just press enter to keep previous data
-            //TODO: Send to BLL passthrough to data with each iteration (in loop)
-            //TODO: Display new (After Edited) order
-            //TODO: Ask if user would like to edit another order
+        //    //In Edit Method
+        //    //TODO: Ask User for a date
+        //    //TODO: Ask User for order
+        //    //TODO: Check to see if date and # are valid (Possibly in validInput(dateTime, int))
+        //    //TODO: Call GetOrder() in DisplayWorkflow
+        //    //TODO: Display the Order recieved
+        //    //TODO: Displays the variables to be edited (Name, State, Product Type, Area
+        //    //TODO: Asks User if they want to edit or just press enter to keep previous data
+        //    //TODO: Send to BLL passthrough to data with each iteration (in loop)
+        //    //TODO: Display new (After Edited) order
+        //    //TODO: Ask if user would like to edit another order
 
-            return null;
 
         }
 
     }
 
-}
+
 
 
