5c81db298932d5d088a1e63484d6faba7dcd5d6a
Eleanor Miller <MillerLElle@gmail.com>
2016-03-15 14:17:01 -0400

    GFEJJ-10 MADE TEST FOR REPO THAT IS CREATED

--- Flooring/Flooring.Tests/Tests.cs
+++ Flooring/Flooring.Tests/Tests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Flooring.Data;
 using NUnit.Framework;
 
 namespace Flooring.Tests
@@ -10,5 +11,18 @@ namespace Flooring.Tests
     [TestFixture]
     public class Tests
     {
+        //testing the floor repo
+        [Test]
+        public void CanLoadAccount()
+        {
+            var repo = new FloorRepository();
+
+            var orders = repo.GetAllOrders();
+            DateTime orderDate = DateTime.Parse("01/14/1992");
+
+            Assert.AreEqual(1, orders[orderDate].OrderNumber );
+            Assert.AreEqual("Elle Miller", orders[orderDate].FirstName + " " + orders[orderDate].LastName);
+            Assert.AreEqual(100,orders[orderDate].OrderArea);
+        }
     }
 }
