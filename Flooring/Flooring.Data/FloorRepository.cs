c6e5df34364e021e37bdd3adc2f0aedbc81df218
Hyun Jeong Hong <hisuida@me.com>
2016-03-15 15:11:32 -0400

    GFEJJ-2 Started delete order method

--- Flooring/Flooring.Data/FloorRepository.cs
+++ Flooring/Flooring.Data/FloorRepository.cs
@@ -76,4 +76,4 @@ namespace Flooring.Data
     ////    }
     //}
 }
-}
+
--- Flooring/Flooring.UI/Workflows/EditWorkflow.cs
+++ Flooring/Flooring.UI/Workflows/EditWorkflow.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
+using Flooring.BLL.OrderOperations;
 
 namespace Flooring.UI.Workflows
 {
@@ -108,7 +109,27 @@ namespace Flooring.UI.Workflows
 
         public void DeleteOrder()
         {
+        
+            OrderOperations op = new OrderOperations();
 
+            Console.Write("Are you sure you want to delete this order? (Yes/No): ");
+            string input = Console.ReadLine();
+            if (input.ToUpper() == "YES")
+            {
+                op.DeleteOrder(order);
+                Console.WriteLine("Press enter to continue... Goodbye!");
+                Console.ReadLine();
+                MainMenu back = new MainMenu();
+                back.Display();
+                return;
+            }
+            if (input.ToUpper() == "NO")
+            {
+                return;
+            }
+            Console.WriteLine("ERROR: That was not a valid input!");
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
 
         }
 
