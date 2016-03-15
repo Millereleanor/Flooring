99158a0b3c5ec43282a29d2da1e488e12b0ead27
Eleanor Miller <MillerLElle@gmail.com>
2016-03-15 16:30:03 -0400

    EFEJJ-10 (not yelling) fixed repo test and working on writing to file

--- Flooring/Flooring.Data/FloorRepository.cs
+++ Flooring/Flooring.Data/FloorRepository.cs
@@ -11,8 +11,9 @@ namespace Flooring.Data
 {
     public class FloorRepository
     {
-        
 
+        //should used dictionary??
+        //get file name??
         public Dictionary<DateTime, Order> GetAllOrders()
         {
             Dictionary<DateTime, Order> order = new Dictionary<DateTime, Order>();
@@ -20,7 +21,7 @@ namespace Flooring.Data
             string filePath = ConfigurationManager.AppSettings["FileName"];
 
 
-           
+
 
             var reader = File.ReadAllLines(filePath);
             for (int i = 1; i < reader.Length; i++)
@@ -43,37 +44,37 @@ namespace Flooring.Data
                 newOrder.TaxRate = decimal.Parse(columns[4]);
                 newOrder.TaxTotal = decimal.Parse(columns[9]);
                 newOrder.OrderTotal = decimal.Parse(columns[10]);
-                
+
 
                 order.Add(DateTime.Parse("01/14/1992"), newOrder);
             }
             return order;
         }
+        //dictionary???
+        public void WriteToFile(Dictionary<DateTime,Order> Order)
+        {
+            string filePath = ConfigurationManager.AppSettings["FileName"];
+            using (StreamWriter writer = new StreamWriter(filePath, false))
+            {
+                {
+                    writer.WriteLine("");
 
-
+                    foreach (var order in Order)
+                    {
+                       // writer.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10}", order, order.OrderArea, order.OrderDate);
+                    }
+                }
+                //    }
+            }
+        }
     }
-     //todo:how do i write the date as the file name;
-            //check if file exit
-            //if not then make new one
-            //if yes then update file  to a new line
-//do this with dictinary...
-                //Order.Add(Order);
+    //todo:how do i write the date as the file name;
+    //check if file exit
+    //if not then make new one
+    //if yes then update file  to a new line
+    //do this with dictinary...
+    //Order.Add(Order);
 
-    //refrence from banking app.....
-    //public void WriteToFile(List<Order> orders)
-    //{
-    //    string filePath = ConfigurationManager.AppSettings["FileName"];
-    //    using (StreamWriter writer = new StreamWriter(filePath, false))
-    //    {
-    //        {
-    //            writer.WriteLine("");
+//refrence from banking app.....
 
-    //            foreach (var order in orders)
-    //            {
-    //                writer.WriteLine("{0},{1},{2},{3}", order.OrderNumber,order.OrderArea,order.OrderDate);
-    //            }
-    //        }
-    ////    }
-    //}
-}
 
