16af48cadbf08a932e7cc2d18d97948b57a9764f
Hyun Jeong Hong <hisuida@me.com>
2016-03-16 11:27:25 -0400

    GFEJJ-1 main menu fix

--- Flooring/Flooring.UI/Workflows/MainMenuDisplay.cs
+++ Flooring/Flooring.UI/Workflows/MainMenuDisplay.cs
@@ -50,14 +50,15 @@ namespace Flooring.UI.Workflows
                     orderDisplay.Execute();
                     break;
                 case "2":
-
+                    //AddOrderWorkflow 
+                    
+                    
+                case "3":
+                    
+                case "4":
                     Console.WriteLine("This feature is not implemented yet!");
                     Console.WriteLine("Press enter to continue");
                     Console.ReadLine();
-                    break;
-                case "3":
-                    break;
-                case "4":
                     break;
                 default:
                     Console.WriteLine("{0} is not valid", choice);
