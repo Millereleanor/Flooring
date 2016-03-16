d3858053cdabea5eed97b7819c1adfc97efde1e4
Hyun Jeong Hong <hisuida@me.com>
2016-03-16 10:16:47 -0400

    GFEJJ-2 display multiple orders

--- Flooring/Flooring.UI/Workflows/DisplayOrderWorkflow.cs
+++ Flooring/Flooring.UI/Workflows/DisplayOrderWorkflow.cs
@@ -29,7 +29,7 @@ namespace Flooring.UI.Workflows
             do
             {
                 Console.Clear();
-                Console.Write("Please enter the order date: ");
+                Console.Write("Please enter the order date(MM/DD/YYYY or MM-DD-YYYY): ");
                 string dateoforder = Console.ReadLine();
 
                 if (DateTime.TryParse(dateoforder, out Date))
@@ -53,7 +53,9 @@ namespace Flooring.UI.Workflows
             if (response.Success)
             {
                 _currentOrder = response.OrderInfo;
-                PrintOrderInformation(response.OrderList);
+                
+                
+                PrintOrderInformation(response);
 
             }
             else
@@ -64,25 +66,30 @@ namespace Flooring.UI.Workflows
                 Console.ReadLine();
             }
         }
-        private void PrintOrderInformation(List<Order>orderList )
+        private void PrintOrderInformation(Response response)
         {
             
             Console.Clear();
-            foreach (var order in orderList)
+            Console.WriteLine("Order Date: {0}", Date.ToShortDateString());
+            Console.WriteLine("{0} order(s) found on this date. ",response.OrderList.Count);
+            Console.WriteLine("******************************************************");
+            Console.WriteLine();
+            foreach (var order in response.OrderList)
             {
                 Console.WriteLine("Order Information: ");
                 Console.WriteLine("=====================================");
                 Console.WriteLine("Order Number: {0}", order.OrderNumber);
                 Console.WriteLine("Customer Name: {0},{1}", order.LastName, order.FirstName);
-                Console.WriteLine("Order State: {0}, ({1})      State Tax Rate: {2}",
+                Console.WriteLine("Order State: {0} ({1})          State Tax Rate: {2:P}",
                     order.StateAbbr, order.StateFull, order.TaxRate);
-                Console.WriteLine("Product Type: {0}     Order Area: {1}", order.ProductType, order.OrderArea);
+                Console.WriteLine("Product Type: {0}                  Order Area: {1}", order.ProductType, order.OrderArea);
                 Console.WriteLine("Material Cost per Ft^2: {0}        Labor Cost per Ft^2: {1}",
                     order.CostperSqFt, order.LaborperSqFt);
                 Console.WriteLine("Order Total: {0:C}", order.OrderTotal+order.TaxTotal);
                 Console.WriteLine();
-                Console.ReadLine();
+                
             }
+            Console.ReadLine();
         }
 
 
