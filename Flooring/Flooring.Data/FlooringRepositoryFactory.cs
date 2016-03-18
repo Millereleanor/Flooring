587f979634b9ca19090d9d0f5a23771c62b258ee
John Palazzo <jpalazzo225@gmail.com>
2016-03-18 14:03:28 -0400

    GFEJJ-1 added factory.. still needs finished

---
+++ Flooring/Flooring.Data/FlooringRepositoryFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flooring.Data
+{
+    public static class FlooringRepositoryFactory
+    {
+        public static IFloorRepository CreateFloorRepository(string type)
+        {
+            //TODO: THIS
+            return new FloorRepository();
+        }
+    }
+}
